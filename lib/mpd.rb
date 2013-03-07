eda51978f88d8058698ebd8d041fe8667b8d831f
Mathias Söderberg <mathias.soederberg@gmail.com>
2013-03-07 23:29:27 +0100

    Remove ALL THE THINGS

--- lib/mpd.rb
+++ lib/mpd.rb
@@ -1,18 +1,2 @@
-require 'mpd/protocol'
-
 module MPD
-
-	def self.new(*args)
-		Client.new(*args)
-	end
-
-	class Client
-		include MPD::Protocol
-
-		def initialize(options = {})
-			self.host = options[:host] || DEFAULT_HOST
-			self.port = options[:port] || DEFAULT_PORT
-		end
-
-	end
 end
